3f88ad28dc421d1c4dfa4e723bc5f6aefd15debf
hdyar <bloop@hdyar.com>
2022-12-13 19:31:12 -0500

    Fixed sprite flashing, added repeat flashing.

--- Assets/Scripts/Animation/Procedural/AnimationExtensions.cs
+++ Assets/Scripts/Animation/Procedural/AnimationExtensions.cs
@@ -7,13 +7,27 @@ namespace RogueDescent.Animation
 		/// <summary>
 		/// Starts the flash coroutine in the SRProcAnim class.
 		/// </summary>
-		/// <param name="spriteRenderer"></param>
+		/// <param name="spriteRenderer">Sprite to flash</param>
 		/// <param name="context">The object to call 'StartCoroutine' on. Probably should be "this".</param>
 		/// <param name="color">Change the sprite renderer color to this.</param>
 		/// <param name="duration">Duration of flash. Use 0 for a single frame.</param>
 		public static void Flash(this SpriteRenderer spriteRenderer, MonoBehaviour context, Color color, float duration = 0)
 		{
-			context.StartCoroutine(SRProcAnim.Flash(spriteRenderer,color, duration));
+			context.StartCoroutine(SRProcAnim.FlashShader(spriteRenderer,color, duration));
+		}
+
+		/// <summary>
+		/// Flash a sprite multiple times.
+		/// </summary>
+		/// <param name="spriteRenderer">Sprite to flash</param>
+		/// <param name="context">The object to call 'StartCoroutine' on. Probably should be "this".</param>
+		/// <param name="color">The color will turn to this, using a shader with "_IgnoreTexture" and "_Color" properties, and ignoring the sprite renderer multiply color (set to white during flash)</param>
+		/// <param name="onDuration">Time the sprite will be this color for each flash</param>
+		/// <param name="offDuration">Time in between flashes.</param>
+		/// <param name="cycles">Number of times to flash.</param>
+		public static void FlashRepeated(this SpriteRenderer spriteRenderer, MonoBehaviour context, Color color, float onDuration,float offDuration,int cycles)
+		{
+			context.StartCoroutine(SRProcAnim.FlashShaderRepeated(context,spriteRenderer, color, onDuration,offDuration,cycles));
 		}
 	}
 }
--- Assets/Scripts/Animation/Procedural/SRProcAnim.cs
+++ Assets/Scripts/Animation/Procedural/SRProcAnim.cs
@@ -8,10 +8,10 @@ namespace RogueDescent.Animation
 	/// </summary>
 	public static class SRProcAnim
 	{
-		private static readonly int ColorProp = Shader.PropertyToID("Color");
-		private static readonly int IgnoreTexture = Shader.PropertyToID("IgnoreTexture");
+		public static readonly int ColorProp = Shader.PropertyToID("_Color");
+		public static readonly int IgnoreTexture = Shader.PropertyToID("_IgnoreTexture");
 
-		/// <param name="spriteRenderer">SpriteRenderer to flash.</param>
+		/// <param name="spriteRenderer">SpriteRenderer to flash. Only adjusts the Sprite Renderer "Color" Property.</param>
 		/// <param name="color">Color to flash.</param>
 		/// <param name="duration">Use 0 for a single frame, ignorant of framerate. Recommended to hard-set this to a value.</param>
 		public static IEnumerator Flash(SpriteRenderer spriteRenderer,Color color, float duration)
@@ -34,11 +34,19 @@ namespace RogueDescent.Animation
 		public static IEnumerator FlashShader(SpriteRenderer spriteRenderer, Color color, float duration)
 		{
 			//a proper flash will probably modify a shader. We will get to that soon using ShaderGraph!
+			//cache material
 			Material mat = spriteRenderer.material;
+			//store default references to return to after flashing
+			var srColor = spriteRenderer.color;
 			Color defaultColor = mat.GetColor(ColorProp);
+			
+			//adjust colors
+			//un-adjust any character adjustments to colors so the sprite color is pure.
+			spriteRenderer.color = Color.white;
 			mat.SetColor(ColorProp,color);
 			mat.SetFloat(IgnoreTexture, 1);
 			
+			//delay. The flash.
 			if (duration > 0)
 			{
 				yield return new WaitForSeconds(duration);
@@ -48,8 +56,22 @@ namespace RogueDescent.Animation
 				yield return new WaitForEndOfFrame();
 			}
 
+			//reset colors
+			spriteRenderer.color = srColor;
 			mat.SetFloat(IgnoreTexture, 0);
 			mat.SetColor(ColorProp, defaultColor);
+		}
+
+		public static IEnumerator FlashShaderRepeated(MonoBehaviour context, SpriteRenderer spriteRenderer, Color color, float flashOnDuration, float flashOffDuration, int flashCycles)
+		{
+			for (int i = 0; i < flashCycles; i++)
+			{
+				yield return context.StartCoroutine(FlashShader(spriteRenderer, color, flashOnDuration));
+				if (i < flashCycles - 1)
+				{
+					yield return new WaitForSeconds(flashOffDuration);
+				}
+			}
 		}
 	}
 }
